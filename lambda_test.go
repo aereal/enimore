da32b61269565f295aec341c7fcc40327edba872
aereal <aereal@aereal.org>
2021-08-28 11:25:15 +0900

    test: add successful case

--- lambda_test.go
+++ lambda_test.go
@@ -16,6 +16,41 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+func TestLambdaFunctionAccumulate_ok(t *testing.T) {
+	fnARN := "arn:aws:lambda:us-east-1:123456789012:function/my-fn"
+	securityGroupIDs := []string{"sg-1234567890", "sg-987654321"}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mlc := mocks.NewMockLambdaClient(ctrl)
+	mlc.EXPECT().ListFunctions(gomock.Any(), gomock.Any()).Times(1).Return(&lambda.ListFunctionsOutput{
+		Functions: []lambdatypes.FunctionConfiguration{
+			{
+				FunctionArn: &fnARN,
+				VpcConfig: &lambdatypes.VpcConfigResponse{
+					SecurityGroupIds: securityGroupIDs,
+				},
+			},
+		},
+	}, nil)
+	mec := mocks.NewMockEC2Client(ctrl)
+	mec.EXPECT().DescribeNetworkInterfaces(gomock.Any(), gomock.Any()).Times(1).Return(&ec2.DescribeNetworkInterfacesOutput{
+		NetworkInterfaces: []ec2types.NetworkInterface{
+			{
+				NetworkInterfaceId: aws.String("eni-12345"),
+				Groups:             []ec2types.GroupIdentifier{{GroupId: &securityGroupIDs[0]}, {GroupId: &securityGroupIDs[1]}},
+				AvailabilityZone:   aws.String("us-east-1a"),
+			},
+		},
+	}, nil)
+	p := enipopulator.New(mec)
+	a := NewLambdaFunctionAccumulator(mlc, []arn.ARN{mustParseARN(fnARN)})
+	ctx := context.Background()
+	err := a.Accumulate(ctx, p)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestLambdaFunctionAccumulate_notVPC(t *testing.T) {
 	fnARN := "arn:aws:lambda:us-east-1:123456789012:function/no-vpc"
 	ctrl := gomock.NewController(t)
